08b992a8ccbea66d34122d9435a33cdf09c9a224
noby <n.fujioka@abacusmaths.info>
2016-08-05 22:36:04 +0100

    fixed issues

--- app/app.rb
+++ app/app.rb
@@ -6,23 +6,23 @@ require 'json' #built-in gem to parse JSON
 require_relative 'data_mapper_setup'
 
 class Thermostat < Sinatra::Base
-  get '/temperature.json' do
+  get '/thermoinfo.json' do
     headers 'Access-Control-Allow-Origin' => '*'
     tempFromDatabase = Temperature.last.temperature
-    { temperature: tempFromDatabase, city: "London" }.to_json
+    cityFromDatabase = City.last.city
+    { temperature: tempFromDatabase, city: cityFromDatabase }.to_json
   end
 
   post '/temperatureToServer.json' do
     headers 'Access-Control-Allow-Origin' => '*'
     Temperature.create(temperature: params[:temperature])
-    puts "temperature from database: #{Temperature.last.temperature}"
-    'hello'
+    "temperature from database: #{Temperature.last.temperature}"
   end
 
   post '/cityToServer.json' do
     headers 'Access-Control-Allow-Origin' => '*'
-    Temperature.create(city: params[:city])
-    'hello'
+    City.create(city: params[:city])
+    "city from database: #{City.last.city}"
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
--- src/interface.js
+++ src/interface.js
@@ -1,24 +1,34 @@
 'use strict';
 
 var thermostat = new Thermostat();
-$.getJSON('http://localhost:9292/temperature.json', function(data) {
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city;
+  var token = '&appid=8e081a97ce3ce35b04ff7460e037fc27';
+  var units = '&units=metric';
+  $.get(url+token+units, function(data) {
+  $('#current-temperature').text(city + " " + data.main.temp);
+  });
+}
+
+$.getJSON('http://localhost:9292/thermoinfo.json', function(data) {
     thermostat.temperature = data.temperature;
+    displayWeather(data.city);
     console.log("json temp: ", thermostat.temperature, "json city: ", data.city);
 });
 
 $( document ).ready(function() {
-  function displayWeather(city) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city;
-    var token = '&appid=8e081a97ce3ce35b04ff7460e037fc27';
-    var units = '&units=metric';
-    $.get(url+token+units, function(data) {
-    $('#current-temperature').text(data.main.temp);
+
+
+  function sendCity(city) {
+    $.post('http://localhost:9292/cityToServer.json', {city: city }, function(data){
+      console.log("post success! call back returned!", data);
     });
   }
 
   $('#select-city').submit(function(event){
     event.preventDefault();
     var city = $("#current-city").val();
+    sendCity(city);
     displayWeather(city);
   });
 
